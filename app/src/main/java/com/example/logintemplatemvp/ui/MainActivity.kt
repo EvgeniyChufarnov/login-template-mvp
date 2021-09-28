ffe3d5c53c1e73d50a91310512a7b8c6e84b36b0
Evgeniy Chufarnov <evgeniychufarnov@gmail.com>
2021-09-28 20:52:08 +0300

    changed main activity layout

--- app/src/main/java/com/example/logintemplatemvp/ui/MainActivity.kt
+++ app/src/main/java/com/example/logintemplatemvp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.logintemplatemvp
+package com.example.logintemplatemvp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.logintemplatemvp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
